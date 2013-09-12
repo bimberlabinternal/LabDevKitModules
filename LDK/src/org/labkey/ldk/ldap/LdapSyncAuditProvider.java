1bf0741d2a7567af12e10ff30505969cedebc5be
Kevin Krouse <kevink@labkey.com>
2013-09-12 02:29:18 +0000

    change audit domains to use property descriptors instead of base properties
    SVN r28329 |2013-09-12 02:29:18 +0000

--- LDK/src/org/labkey/ldk/ldap/LdapSyncAuditProvider.java
+++ LDK/src/org/labkey/ldk/ldap/LdapSyncAuditProvider.java
@@ -8,22 +8,21 @@ import org.labkey.api.audit.query.AbstractAuditDomainKind;
 import org.labkey.api.audit.query.DefaultAuditTypeTable;
 import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.DbSchema;
-import org.labkey.api.data.JdbcType;
-import org.labkey.api.data.PropertyStorageSpec;
 import org.labkey.api.data.TableInfo;
+import org.labkey.api.exp.PropertyDescriptor;
+import org.labkey.api.exp.PropertyType;
 import org.labkey.api.exp.property.Domain;
-import org.labkey.api.exp.property.DomainKind;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.UserSchema;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * Created by IntelliJ IDEA.
  * User: klum
  * Date: 8/12/13
  */
@@ -99,7 +98,7 @@ public class LdapSyncAuditProvider extends AbstractAuditTypeProvider implements 
     }
 
     @Override
-    protected DomainKind getDomainKind()
+    protected AbstractAuditDomainKind getDomainKind()
     {
         return new LdapSyncAuditDomainKind();
     }
@@ -189,21 +188,21 @@ public class LdapSyncAuditProvider extends AbstractAuditTypeProvider implements 
         public static final String NAME = "LdapSyncAuditDomain";
         public static String NAMESPACE_PREFIX = "Audit-" + NAME;
 
-        private static final Set<PropertyStorageSpec> _fields = new LinkedHashSet<>();
-
-        static {
-            _fields.add(createFieldSpec(COLUMN_NAME_USERS_AND_GROUPS_ADDED, JdbcType.INTEGER));
-            _fields.add(createFieldSpec(COLUMN_NAME_USERS_AND_GROUPS_REMOVED, JdbcType.INTEGER));
-            _fields.add(createFieldSpec(COLUMN_NAME_MEMBERSHIPS_CHANGED, JdbcType.INTEGER));
-        }
+        private final Set<PropertyDescriptor> _fields;
 
         public LdapSyncAuditDomainKind()
         {
             super(LdapSyncRunner.AUDIT_EVENT_TYPE);
+
+            Set<PropertyDescriptor> fields = new LinkedHashSet<>();
+            fields.add(createPropertyDescriptor(COLUMN_NAME_USERS_AND_GROUPS_ADDED, PropertyType.INTEGER));
+            fields.add(createPropertyDescriptor(COLUMN_NAME_USERS_AND_GROUPS_REMOVED, PropertyType.INTEGER));
+            fields.add(createPropertyDescriptor(COLUMN_NAME_MEMBERSHIPS_CHANGED, PropertyType.INTEGER));
+            _fields = Collections.unmodifiableSet(fields);
         }
 
         @Override
-        protected Set<PropertyStorageSpec> getColumns()
+        public Set<PropertyDescriptor> getProperties()
         {
             return _fields;
         }
