c0b633119afc83f2cfb4d7c9f0c355ade9d50a3b
bbimber <bbimber@gmail.com>
2021-01-13 14:14:56 -0800

    Reduce threshold for switching to POST

--- laboratory/resources/web/laboratory/window/WorkbookCreationWindow.js
+++ laboratory/resources/web/laboratory/window/WorkbookCreationWindow.js
@@ -126,7 +126,7 @@ Ext4.define('Laboratory.panel.WorkbookCreationPanel', {
         var urlParams = this.urlParams || {};
 
         var url = LABKEY.ActionURL.buildURL(controller, action, containerPath, urlParams);
-        if (url.length > 1200){
+        if (url.length > 1000){
             Ext4.create('Ext.form.Panel', {
                 url: LABKEY.ActionURL.buildURL(controller, action, containerPath),
                 method: 'POST',
