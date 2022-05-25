fa8af05fa35cfa319f4e2f61c276c8f693f07672
bbimber <bbimber@gmail.com>
2022-05-25 14:28:06 -0700

    Improve permission checking in DemographicsSources

--- laboratory/src/org/labkey/laboratory/LaboratoryServiceImpl.java
+++ laboratory/src/org/labkey/laboratory/LaboratoryServiceImpl.java
@@ -383,7 +383,9 @@ public class LaboratoryServiceImpl extends LaboratoryService
             {
                 DemographicsSource source = DemographicsSource.getFromPropertyManager(target, u, key, properties.get(key));
                 if (source != null)
+                {
                     qds.add(source);
+                }
             }
             catch (IllegalArgumentException e)
             {
@@ -450,18 +452,26 @@ public class LaboratoryServiceImpl extends LaboratoryService
         {
             Container c = ContainerManager.getForId(entry.getObjectId());
             if (c == null || !c.hasPermission(u, ReadPermission.class))
+            {
                 continue;
+            }
 
             Set<DemographicsSource> set = map.get(c);
             if (set == null)
+            {
                 set = new HashSet<>();
+            }
 
             DemographicsSource source = DemographicsSource.getFromPropertyManager(c, u, entry.getKey(), entry.getValue());
             if (source != null)
+            {
                 set.add(source);
+            }
 
-            if (set.size() > 0)
+            if (!set.isEmpty())
+            {
                 map.put(c, set);
+            }
         }
 
         return Collections.unmodifiableMap(map);
